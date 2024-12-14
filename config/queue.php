1e7dd2ccc22f6c76857780a3344710c31bb26ac3
Milena <milena.devweb@gmail.com>
2024-12-14 07:23:33 -0300

    Foi realizada as configurações do rabbitmq

--- config/queue.php
+++ config/queue.php
@@ -71,6 +71,22 @@ return [
             'after_commit' => false,
         ],
 
+        'rabbitmq' => [
+
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+            'worker' => env('RABBITMQ_WORKER', 'default'),
+        ],
+
     ],
 
     /*
