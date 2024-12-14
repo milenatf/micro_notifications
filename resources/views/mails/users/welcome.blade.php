a21ed53cab23553676acbdb1e7ae3bed095308c9
Milena <milena.devweb@gmail.com>
2024-12-14 16:21:44 -0300

    Foi criado o email de boas vindas ao usuário registrado

---
+++ resources/views/mails/users/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+# Olá
+
+Seja bem vindo(a) ao nosso sistema.
+
+Obrigado,<br>
+{{ config('app.name') }}
+</x-mail::message>
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Jobs\UserRegisteredJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/teste', function() {
+
+    UserRegisteredJob::dispatch('milena.devweb@gmail.com')->onQueue('queue_notification');
+
+    return response()->json(['status' => 'sucess', 'message' => 'Send email.']);
 });
 
 Route::get('/', function() {
