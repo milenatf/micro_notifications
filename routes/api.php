682b3614cabb99e7fb6ee8f77e3e9e2ce62a9f78
Milena <milena.devweb@gmail.com>
2024-12-14 06:40:22 -0300

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', function() {
+    return response()->json(['status' => 'sucess', 'message' => 'Micro notification is runing.']);
+});
